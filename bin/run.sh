2e96d70ad5fd1154f3a3e49fa826f27942ae8895
smartrics <fabrizio.cannizzo@iotic-labs.com>
2022-12-16 10:48:17 +0000

    added some documentation

---
+++ bin/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+HOME="../target/"
+JAVA_OPTS="${JAVA_OPTS} -Djava.library.path=./lib"
+
+CP="${HOME}/dadsjokes-1.0-SNAPSHOT.jar"
+CP="${CP}:${HOME}/lib/*"
+
+MAIN_CLASS="smartrics.iotics.connector.Main"
+
+java "${JAVA_OPTS}" -classpath "${CP}" ${MAIN_CLASS}
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-java -Djava.library.path=./lib -classpath "target/dadsjokes-1.0-SNAPSHOT.jar:target/lib/*" smartrics.iotics.connector.Main
